9463714417991991682f363c1f69fa7912b316be
Walter Cumbucane <cumbucane.walter@gmail.com>
2024-05-22 13:54:22 +0100

    User Interface added

--- stock_management/src/database/Main.java
+++ stock_management/src/database/Main.java
@@ -5,6 +5,7 @@
 package database;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 /**
  *
@@ -15,12 +16,54 @@ public class Main {
     public static void main(String[] args){
         
         Connection conn = Operations.Connect();
-        Product produto = new Product("novo", "Java Handled", 5, 15);
-        //Operations.insertProduct(produto1, conn);
-        Operations.readProducts(conn);
-        Operations.deleteProduct(2, conn);
-        Operations.deleteProduct(1, conn);
-        System.out.println("After product deletion");
-        Operations.readProducts(conn);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Bem-vindo ao sistema de Gerenciamento de stocks.");
+        System.out.println("Que operacoes deseja efectuar: ");
+        System.out.println("1- Listar Produtos disponiveis no stock");
+        System.out.println("2- Adicionar um novo produto ao stock");
+        System.out.println("3- Actualizar um produto existente no stock");
+        System.out.println("4- Eliminar um produto existente no stock");
+        System.out.println("5- Sair do sistema");
+        System.out.print("Escolha: ");
+        int opcao = scanner.nextInt();
+        System.out.println("--------------------------------------");
+        System.out.println("--------------------------------------");
+        
+        while (opcao != 5) {
+            
+            if (opcao == 1){ 
+                UserInterface.listarProdutos(conn);
+            } 
+            else if (opcao == 2) {
+                UserInterface.adicionarProduto(conn);
+            }
+            else if (opcao == 3) {
+                UserInterface.actualizarProdutos(conn);
+            }
+            else if (opcao == 4) {
+                UserInterface.eliminarProduto(conn);
+            }
+            else {
+                System.out.println("Por favor, introduza uma opcao valida!");
+            }
+            
+            System.out.println("--------------------------------------");
+            System.out.println("--------------------------------------");
+            System.out.println("Que operacoes deseja efectuar: ");
+            System.out.println("1- Listar Produtos disponiveis no stock");
+            System.out.println("2- Adicionar um novo produto ao stock");
+            System.out.println("3- Actualizar um produto existente no stock");
+            System.out.println("4- Eliminar um produto existente no stock");
+            System.out.println("5- Sair do sistema");
+            System.out.print("Escolha: ");
+            opcao = scanner.nextInt();
+            System.out.println("--------------------------------------");
+            System.out.println("--------------------------------------");
+            
+         
+        }
+       
+        
     }
 }
--- stock_management/src/database/UserInterface.java
+++ stock_management/src/database/UserInterface.java
@@ -3,11 +3,63 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package database;
+import java.util.Scanner;
+import java.sql.Connection;
 
 /**
  *
  * @author walter
  */
 public class UserInterface {
+    
+    public static void adicionarProduto(Connection conn){
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Qual e o nome do produto que deseja inserir: ");
+        String name = scanner.nextLine();
+        System.out.println("Insira uma descricao para o produto que deseja armazenar: ");
+        String description = scanner.nextLine();
+        System.out.println("Qual e a quantidade unitaria de produtos disponvel: ");
+        int quantity = scanner.nextInt();
+        System.out.println("Qual e o preco unitario de cada produto: ");
+        double price = scanner.nextDouble();
+        
+        Product produto = new Product(name, description, quantity, price);
+        Operations.insertProduct(produto, conn);
+    }
+    
+    public static void listarProdutos(Connection conn) {
+        
+        Operations.readProducts(conn);
+    }
+    
+    public static void actualizarProdutos(Connection conn) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Qual e o id do produto que deseja actualizar: ");
+        int productId = Integer.parseInt(scanner.nextLine());
+        System.out.println("Insira um novo nome para o produto: ");
+        String name = scanner.nextLine();
+        System.out.println("Insira uma nova descricao para o produto: ");
+        String description = scanner.nextLine();
+        System.out.println("Insira uma nova quantidade unitaria para o produto: ");
+        int quantity = scanner.nextInt();
+        System.out.println("Insira um novo preco unitario para o produto: ");
+        double price = scanner.nextDouble();
+        
+        Product produto = new Product(name, description, quantity, price);
+        Operations.updateProduct(produto, productId, conn);
+    }
+    
+    public static void eliminarProduto(Connection conn) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Qual e o id do produto que deseja eliminar: ");
+        int productId = scanner.nextInt();
+        Operations.deleteProduct(productId, conn);
+    }
     
 }
